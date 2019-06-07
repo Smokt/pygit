a57b33fdb9e582978d70cf2efc5e4e9a08c00663
Alejandro Martinez Andres <ama1081996@gmail.com>
2019-06-07 10:06:34 +0200

    find_objects & encode & create pack

--- pygit.py
+++ pygit.py
@@ -349,4 +349,88 @@ def get_remote_master_hash(git_url, username, password):
     master_sha1, master_ref = lines[2].split(b'\x00')[0].split()
     assert master_ref == b'refs/heads/master'
     return master_sha1.decode()
+def read_tree(sha1=None, data=None):
+    """Read tree object object with given SHA-1 (hex string) or data, and return list
+    of (mode, path, sha1) tuples.
+    """
+    if sha1 is not None:
+        obj_type, data = read_object(sha1)
+        assert obj_type == 'tree'
+    elif data is None:
+        raise TypeError('must specify "sha1" or "data"')
+    i = 0
+    entries = []
+    for _ in range(1000):
+        end = data.find(b'\x00', i)
+        if end == -1:
+            break
+        mode_str, path = data[i:end].decode().split()
+        mode = int(mode_str, 8)
+        digest = data[end + 1:end + 21]
+        entries.append((mode, path, digest.hex()))
+        i = end + 1 + 20
+    return entries
 
+def find_tree_objects(tree_sha1):
+    """Return set of SHA-1 hashes of all objects in this tree (recursively),
+    incluiding the hash of the tree itself.
+    """
+    objects = {tree_sha1}
+    for mode, path, sha1 in read_tree(sha1=tree_sha1):
+        if stat.S_ISDIR(mode):
+            objects.update(find_tree_objects(sha1))
+        else:
+            objects.add(sha1)
+    return objects
+
+def find_commit_objects(commit_sha1):
+    """Return set of SHA-1 hashes of all objects in this commit (recursively),
+    its tree, its parents, and the hash of the commit itself.
+    """
+    objects = {commit_sha1}
+    obj_type, commit = read_object(commit_sha1)
+    assert obj_type == 'commit'
+    lines = commit.decode().splitlines()
+    tree = next(l[5:45] for l in lines if l.startswith('tree '))
+    objects.update(find_tree_objects(tree))
+    parents = (l[7:47] for l in lines if l.startswith('parent '))
+    for parent in parents:
+        objects.update(find_commit_objects(parent))
+    return objects
+
+def find_missing_objects(local_sha1, remote_sha1):
+    """Return set of SHA-1 hashes of objects in local commit that are missing
+    at the remote (based on the given remote commit hash).
+    """
+    local_objects = find_commit_objects(local_sha1)
+    if remote_sha1 is None:
+        return local_objects
+    remote_objects = find_commit_objects(remote_sha1)
+    return local_objects - remote_objects
+
+def encode_pack_objects(obj):
+    """Encode a single object for a pack file and return bytes (variable-
+    length header followed by compressed data bytes).
+    """
+    obj_type, data = read_object(obj)
+    type_num = ObjectType[obj_type].value
+    size = len(data)
+    byte = (type_num << 4) | (size & 0x0f)
+    size >>= 4
+    header = []
+    while size:
+        header.append(byte | 0x80)
+        byte = size & 0x7f
+        size >>= 7
+    header.append(byte)
+    return bytes(header) + zlib.compress(data)
+
+def create_pack(objects):
+    """Create pack file containing all objects in given set of SHA-1
+    hashes, return data bytes of full pack file.
+    """
+    header = struct.pack('!4sLL', b'PACK', 2, len(objects))
+    body = b''.join(encode_pack_object(o) for o in sorted(objects))
+    sha1 = hashlib.sha1(contents).digest()
+    data = contents + sha1
+    return data
